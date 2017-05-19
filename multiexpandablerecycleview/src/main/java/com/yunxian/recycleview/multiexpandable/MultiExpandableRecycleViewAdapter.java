10bd76a0b99039cfc6f55437ddfcd6b31072505c
A Shuai <ls1110924@gmail.com>
2017-05-19 15:54:23 +0800

    init project

---
+++ multiexpandablerecycleview/src/main/java/com/yunxian/recycleview/multiexpandable/MultiExpandableRecycleViewAdapter.java
@@ -0,0 +1,27 @@
+package com.yunxian.recycleview.multiexpandable;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * @author A Shuai
+ * @email lishuai.ls@alibaba-inc.com
+ * @date 17/5/19 下午3:41
+ */
+public class MultiExpandableRecycleViewAdapter extends RecyclerView.Adapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
